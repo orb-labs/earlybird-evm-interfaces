c46246219cdeaba4e037bd0cb939dde804929a18
Richard Adjei <radedoc@yahoo.com>
2023-12-16 00:31:02 -0500

    updating dispute resolved function to tell us the app

--- src/Libraries/Rukh/RukhReceiveModule/IDisputerContractForRukhReceiveModule.sol
+++ src/Libraries/Rukh/RukhReceiveModule/IDisputerContractForRukhReceiveModule.sol
@@ -11,8 +11,9 @@ pragma solidity ^0.8.17;
 interface IDisputerContractForRukhReceiveModule {
     /**
      * @dev - function returns the amount an oracle is willing to charge for passing a message
+     * @param _app - address indicating the app whose dispute got resolved
      * @param _disputedMsgProofHash - bytes32 indicating the hash of the disputed msg proof.
      * @param _disputeVerdict - uint256 indicating the dispute verdict.
      */
-    function disputeResolved(bytes32 _disputedMsgProofHash, uint256 _disputeVerdict) external;
+    function disputeResolved(address _app, bytes32 _disputedMsgProofHash, uint256 _disputeVerdict) external;
 }
